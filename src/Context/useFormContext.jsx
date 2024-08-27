982e59b85bd64346ce650de0449c6e664ba73fe4
Mirmaxamadov04 <d@mirmaxamadov.gmail.com>
2024-08-27 16:13:18 +0500

    small bud fixed

--- src/Context/useFormContext.jsx
+++ src/Context/useFormContext.jsx
@@ -4,14 +4,14 @@ export const FormContext = React.createContext();
 
 function UseFormContext({ children }) {
   const [formData, setFormData] = React.useState({
-    name: "name",
-    cookTime: "cooktime",
-    servings: "1",
-    instructions: "2",
+    name: "",
+    cookTime: "",
+    servings: "",
+    instructions: "",
     ingredients: [
       {
-        name: "3",
-        amount: "4",
+        name: "",
+        amount: "",
       },
     ],
   });
